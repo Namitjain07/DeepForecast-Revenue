655586c572ad3d5a87242b42a2ce5c6b2f317399
Namitjain07 <jnamit684@gmail.com>
2025-12-09 13:48:25 +0530

    Keep only Website final folder

--- REVENUE/app/src/main/java/com/app/revenue/data/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.app.revenue.data
-
-data class LoginRequest(
-    val username: String,
-    val password: String
-)
--- REVENUE/app/src/main/java/com/app/revenue/data/LoginResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.app.revenue.data
-
-data class LoginResponse(
-    val success: Boolean,
-    val message: String?
-)
